734ccf516fb42b3e92b716640f8799697f4cff04
leonard <leonardgre@gmail.com>
2023-11-29 08:01:55 +0000

    changes

--- src/components/paymentpages/PaySuccess.jsx
+++ src/components/paymentpages/PaySuccess.jsx
@@ -16,33 +16,33 @@ function PaySuccess(props) {
     },
   });
 
-  const onSubmitTransaction = async (start, end) => {
-    const data = getValues();
-    console.log(data);
-    setIsLoading(true);
-    const formatedData = {
-      ...data,
-      // startDate: formatDate(data?.startDate), //sending formated startDate
-      // endDate: formatDate(data?.endDate), //seending formated endDate
-      startDate: start, //sending formated startDate
-      endDate: end, //seending formated endDate
-    };
-    try {
-      // sending api request
-      const transaction = await getTransactions(formatedData);
-      console.log(transaction);
-      setIsLoading(false);
-      setShowTable(true);
-      setTransaction(transaction.translistList); //assigning the reponse to the state
-    } catch (err) {
-      if (err.message) {
-        setIsLoading(false);
-        // toast("Network Error");
-      }
-      console.log(err);
-      toast("Invalid date format");
-    }
-  };
+  // const onSubmitTransaction = async (start, end) => {
+  //   const data = getValues();
+  //   console.log(data);
+  //   setIsLoading(true);
+  //   const formatedData = {
+  //     ...data,
+  //     // startDate: formatDate(data?.startDate), //sending formated startDate
+  //     // endDate: formatDate(data?.endDate), //seending formated endDate
+  //     startDate: start, //sending formated startDate
+  //     endDate: end, //seending formated endDate
+  //   };
+  //   try {
+  //     // sending api request
+  //     const transaction = await getTransactions(formatedData);
+  //     console.log(transaction);
+  //     setIsLoading(false);
+  //     setShowTable(true);
+  //     setTransaction(transaction.translistList); //assigning the reponse to the state
+  //   } catch (err) {
+  //     if (err.message) {
+  //       setIsLoading(false);
+  //       // toast("Network Error");
+  //     }
+  //     console.log(err);
+  //     toast("Invalid date format");
+  //   }
+  // };
   return (
     <div>
       <h1 className="text-center text-success">Payment Initiated</h1>
