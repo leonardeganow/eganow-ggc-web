ed4fda90a5eaaf3f9b30554045fa3f52d32c0b7d
dekin <dekinlinux@gmail.com>
2023-12-11 16:48:58 +0000

    fix card loader

--- src/components/paymentpages/PaySuccess.jsx
+++ src/components/paymentpages/PaySuccess.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { FaCheckCircle } from "react-icons/fa";
 
 function PaySuccess(props) {
   const transType = props.formHandler.getValues("paymentMethod");
+  const [loader,setLoader] = useState(true)
+  console.log(loader)
   const payRef = useRef();
   // ref.current.innerHTML = `<div id="redirectTo3ds1AcsSimple" xmlns="http://www.w3.org/1999/html"> <iframe id="redirectTo3ds1Frame"  name="redirectTo3ds1Frame" height="100%" width="100%"> </iframe><form id="redirectTo3ds1Form" method="POST" action="https://authentication.cardinalcommerce.com/ThreeDSecure/V2_1_0/CReq" target="redirectTo3ds1Frame"> <input type="hidden" name="creq" value="eyJ0aHJlZURTU2VydmVyVHJhbnNJRCI6ImMzMDQ3YzNlLTM0ZTMtNGYwNy1iNDk2LTc4YjM2Mzk4NjFjZCIsImFjc1RyYW5zSUQiOiJlOWYxOTllZC0zMzkwLTQ4NGUtYWViYy1mYzQwY2Y2N2E0NzYiLCJjaGFsbGVuZ2VXaW5kb3dTaXplIjoiMDUiLCJtZXNzYWdlVHlwZSI6IkNSZXEiLCJtZXNzYWdlVmVyc2lvbiI6IjIuMi4wIn0" />  <input type="hidden" /> </form><script id="authenticate-payer-script"> var e=document.getElementById("redirectTo3ds1Form");if (e) { e.submit(); if (e.parentNode !== null) { e.parentNode.removeChild(e); } } </script></div>`;
   // ref.current.innerHTML = props.formHandler.getValues();
@@ -26,7 +28,7 @@ function PaySuccess(props) {
 
   const paymentUrl = props.formHandler.getValues("paymentUrl");
 
-  const redirectContainerRef = React.useRef();
+  const redirectContainerRef = React.useRef(); //frame contaner
   React.useEffect(() => {
     if (redirectContainerRef.current) {
       // Set the HTML content
@@ -41,25 +43,43 @@ function PaySuccess(props) {
         newScript.innerHTML = scriptElement.innerHTML;
         document.body.appendChild(newScript);
 
-      //  const iframeEl=  redirectContainerRef.current.querySelector("iframe");
-      //  if(iframeEl){
-      //    iframeEl.parentNode.style.height = "100%"
-      //    console.log(iframeEl);
-      //  }
-        
-      const divEl = redirectContainerRef.current.querySelector('#redirectTo3ds1AcsSimple')
-      console.log(divEl)
-      if(divEl){
-        divEl.style.height = "100%"
-      }
+        //  const iframeEl=  redirectContainerRef.current.querySelector("iframe");
+        //  if(iframeEl){
+        //    iframeEl.parentNode.style.height = "100%"
+        //    console.log(iframeEl);
+        //  }
 
+        const divEl = redirectContainerRef.current.querySelector('#redirectTo3ds1AcsSimple')
+        console.log(divEl)
+        if (divEl) {
+          divEl.style.height = "100%"
+        }
+
+        const spinner = document.querySelector('#spinner'); //target spinner
+
+        const iFrame = redirectContainerRef.current.querySelector('#redirectTo3ds1Frame') //iframe container
+        if (iFrame) {
+          spinner.style.display = 'block';
+          redirectContainerRef.current.style.display = 'none';
+
+          // Add event listener to detect when iframe has finished loading
+          iFrame.addEventListener('load', () => {
+            // Check if the iframe is loaded from the 3D gateway
+            spinner.style.display = 'none';
+
+            // Show iframe
+            redirectContainerRef.current.style.display = 'block';
+          });
+        }
+
+    
 
 
         // Remove the original script element to avoid duplication
         scriptElement.parentNode.removeChild(scriptElement);
       }
     }
-  }, []);
+  }, [loader]);
 
   return (
     <div className="">
@@ -73,7 +93,14 @@ function PaySuccess(props) {
 
       <h5 className="text-center my-4">
         {transType === "Debit card" ? (
-          <div style={{height: '400px'}} ref={redirectContainerRef}></div>
+          <div>
+            <div id="spinner" style={{ display: 'none' }} className="text-center">
+              <h4>Please Wait for the OTP to complete the transaction</h4>
+              <span  className="spinner-border spinner-border-sm mr-1 text-center"></span>
+            </div> 
+            <div style={{ height: '400px',display:'none' }} ref={redirectContainerRef}></div>
+          </div>
+          
         ) : (
           "You will receive a prompt from your mobile operator to confirm."
         )}
